0e9201ab099ae71f1c49a16e783c5b846519f7f6
alejandroorlaando <aorlando@correo.unimet.edu.ve>
2024-01-31 13:12:50 -0400

    Update Tarea3AlejandroOrlando.py

--- Tarea3AlejandroOrlando.py
+++ Tarea3AlejandroOrlando.py
@@ -1,10 +1,10 @@
-genero = input("¿Cuál es tu genero? (Masculino/Femenino): ").upper()
+genero = input("¿Cuál es tu genero? (M/F): ").upper()
 
-while genero not in ['Masculino', 'Femenino']:
-    print("Por favor, ingresa Masculino o Femenino.")
-    genero = input("¿Cuál es tu genero? (Masculino/Femenino): ").upper()
+while genero not in ['M', 'F']:
+    print("Por favor, ingresa M o F.")
+    genero = input("¿Cuál es tu genero? (M/F): ").upper()
 
-if genero == 'Masculino':
+if genero == 'M':
     genero_definido = "señor"
     saludar = "Bienvenido"
 else:
@@ -12,15 +12,16 @@ else:
     saludar = "Bienvenida"
 
 region = input(f"{saludar} {genero_definido}! ¿De qué región eres?: ")
-pokemon_preferido = input(f"¿Cuál es tu Pokemon preferido? (Venusaur/Ivysaur/Charizard): ").capilize()
+pokemon_preferido = input(f"¿Cuál es tu Pokemon preferido? (Venusaur/Ivysaur/Charizard): ").capitalize()
 
 if pokemon_preferido == "Venusaur":
-    mensaje_de_eleccion = "Buena eleccion, Venusaur mbien es uno de mis preferidos."
-elif pokemon_preferido == "	Ivysaur":
-    mensaje_de_eleccion = "Ivysaur sin duda es un Pokemon inigualable"
+    mensaje_de_eleccion = print("Buena eleccion, Venusaur tambien es uno de mis preferidos.")
+elif pokemon_preferido == "Ivysaur":
+    mensaje_de_eleccion = print("Ivysaur sin duda es un Pokemon inigualable")
 elif pokemon_preferido == "Charizard":
-    mensaje_de_eleccion = "Charizard es un Pokemon muy potente, buena eleccion"
+    mensaje_de_eleccion = print("Charizard es un Pokemon muy potente, buena eleccion")
 else:
-    mensaje_de_eleccion = "No seleccionaste ninguno de los 3 pokemon, ¡pero todos son buenos de todos modos!"
+    mensaje_de_no_eleccion = print("Todos los pokemon son buenos, pero debes escoger entre los 3 señalados")
+    pokemon_preferido = input(f"¿Cuál es tu Pokemon preferido? (Venusaur/Ivysaur/Charizard): ").capitalize()
 
-print(f"\n{saludar} {genero_definido} de {region}, tu Pokemon preferido es {pokemon_preferido}. {mensaje_de_eleccion}")
+print(f"\n{saludar} {genero_definido} de {region}, tu Pokemon preferido es {pokemon_preferido}.")
